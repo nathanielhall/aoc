da58c1a0adeb424187404ec48cc8cbe0b33ef4f0
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-03-05 16:13:17 -0500

    Day 5 - part 1

--- src/05/output.js
+++ src/05/output.js
@@ -1,51 +1,24 @@
+const fs = require("fs");
 
-var readline = require("readline");
+const data = fs.readFileSync(`${__dirname}/input.txt`, "utf-8");
 
-const grid = [];
-const line_counter = (
-  (i = 0) =>
-  () =>
-    ++i
-)();
+const result = data
+  .split("\n")
+  .filter(Boolean)
+  .map((row) => {
+    const rowBinary = row
+      .split("", 7)
+      .map((x) => x.replace("F", 0).replace("B", 1))
+      .join("");
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
+    const colBinary = row
+      .split("")
+      .filter((x) => ["R", "L"].includes(x))
+      .map((x) => x.replace("L", 0).replace("R", 1))
+      .join("");
 
-rl.on("line", function (line, y = line_counter()) {
-
-
-
-
-  grid[y - 1] = line;
-});
-
-rl.on("close", () => {
-  const binarySearch = (code, idx, l, r) => {
-    if (idx === 7) {
-      console.log(`${code[idx]}: ${l} vs ${r}`);
-      if (code[6] === "F") return l;
-      if (code[6] === "B") return r;
-      return "ERROR";
-    }
-    console.log(` char:${code[idx]} idx:${idx} l:${l} r: ${r}`);
-
-    const mid = l + Math.floor((r - l) / 2);
-
-    if (code[idx] === "F") return binarySearch(code, idx + 1, l, mid);
-    if (code[idx] === "B") return binarySearch(code, idx + 1, mid, r);
-
-
-
-    return binarySearch(code, idx + 1, l, r);
-  };
-
-  grid.forEach((row) => {
-    console.log("ROW", row);
-    const result = binarySearch(row, 0, 0, 127);
-
-    console.log(`RESULT`, result);
+    const seatId = parseInt(rowBinary, 2) * 8 + parseInt(colBinary, 2);
+    return seatId;
   });
-});
+
+console.log(Math.max(...result));
