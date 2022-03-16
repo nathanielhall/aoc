7588c17a660757f9fef89cd6b8d9d1d5fe40f723
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-03-16 17:35:07 -0400

    day 1 / part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,5 @@
-use crate::stdx::IterExt;
 pub use crate::command::Command;
+use crate::stdx::IterExt;
 
 pub struct Day1;
 impl Command for Day1 {
@@ -8,24 +8,31 @@ impl Command for Day1 {
     }
     fn execute(&self) {
         let input = include_str!("input.day1.test.txt");
-        let count = parse(input);
 
-        println!("Count: {}", count);
+        let count = part1(&input);
+        println!("Part 1: {:?}", count);
 
-        // let slice = [0, 1, 2, 3];
-        // let iter = slice.array_windows::<2>();
-        // for i in iter {
-        //     println!("Iter: {:?}", i)
-        // }
+        let count = part2(&input);
+        println!("Part 2: {:?}", count)
     }
 }
 
 // https://doc.rust-lang.org/std/slice/struct.ArrayWindows.html
-fn parse(input: &str) -> usize {
+fn part1(input: &str) -> usize {
     input
         .lines()
          .map(|line| line.parse::<u16>().unwrap())
         .array_windows()
+        .filter(|[n1, n2]| n2 > n1)
+        .count()
+}
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line.parse::<u16>().unwrap())
+        .array_windows::<3>()
+        .map(|[n1, n2, n3]| n1 + n2 + n3)
+        .array_windows::<2>()
         .filter(|[n1, n2]| n2 > n1)
         .count()
 }
