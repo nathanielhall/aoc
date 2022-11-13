30d2f1fedb46c48a367009bbd907d11f8ade29e7
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-11-13 05:40:30 -0500

    quick setup cli

---
+++ typescript/src/challenge-template.ts
@@ -0,0 +1,9 @@
+// import data from "./input.txt";
+// import {} from "./utils"
+
+const challenge = () => {
+  // read the input data
+  // parse data
+  // validate
+  return null;
+};
---
+++ typescript/src/utils.ts
@@ -0,0 +1,72 @@
+/*****************************************************************
+ * Advent of Code utilities
+ *
+ * Start a challenge
+ * @example
+ * deno run --allow-net ./src/utils --day 1
+ *****************************************************************/
+import { configSync } from "https://deno.land/std/dotenv/mod.ts";
+import { ensureDir } from "https://deno.land/std/fs/mod.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
+
+const getPuzzleInput = async (day: number, year: number) => {
+  const response = await fetch(
+    `https://adventofcode.com/${year}/day/${day}/input`,
+    {
+      headers: {
+        cookie: configSync().COOKIE,
+      },
+    },
+  );
+  return await response.text();
+};
+
+const exists = async (filename: string) => {
+  try {
+    await Deno.readTextFile(filename);
+    return true;
+  } catch {
+    console.log(`${filename} NOT Exists`);
+    return false;
+  }
+};
+
+async function main() {
+  const flags = parse(Deno.args, {
+    string: ["year", "day"],
+    default: { year: "2020" },
+  });
+
+  if (!flags?.day) {
+    console.error("Must provide a --day flag");
+    return;
+  }
+
+  const day: number = parseInt(flags.day);
+  const year: number = parseInt(flags.year);
+  const path = `./src/${year}/${day}`;
+
+  const inputFilename = `${path}/input.txt`;
+  const challengeFilename = `${path}/challenge.ts`;
+  const templateFilename = `./src/challenge-template.ts`;
+
+  const doScaffold = await exists(inputFilename) === false;
+  if (!doScaffold) {
+    console.warn(`Day ${day} already exists`);
+    return;
+  }
+
+  console.log(`Scaffolding challenge for day #${day} of ${year}`);
+  const data = await getPuzzleInput(day, year);
+
+  try {
+    await ensureDir(path);
+
+    await Deno.writeTextFile(inputFilename, data);
+    await Deno.copyFile(templateFilename, challengeFilename);
+  } catch (err) {
+    console.error("error", err);
+  }
+}
+
+main();
