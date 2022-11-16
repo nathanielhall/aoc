53e8a07d383dd52680aa5e905b3707eeae2885ea
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-11-16 06:41:38 -0500

    2020 day 7 part 1

--- typescript/src/2020/7/challenge.ts
+++ typescript/src/2020/7/challenge.ts
@@ -1,9 +1,42 @@
-// import data from "./input.txt";
-// import {} from "./utils"
+// -----------------------------------------------------------
+//  Day 7: Handy Haversacks
+// -----------------------------------------------------------
+import { breadthFirstSearch, readLines } from "../../utils.ts";
 
-const challenge = () => {
-  // read the input data
-  // parse data
-  // validate
-  return null;
+const example: () => string = () => {
+  return (
+    `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`
+  );
 };
+
+async function main() {
+  const text = await Deno.readTextFile("./input.txt");
+  // const text = example();
+
+  const lookup = readLines(text).extract().reduce((prev, curr) => {
+    const [key, value] = curr.split("contain");
+
+    return ({
+      ...prev,
+      [key.replace("bags", "").trim()]: value,
+    });
+  }, {});
+
+  const results = breadthFirstSearch(lookup, "shiny gold");
+
+  console.log(
+    `Answer is ${results.length} - Total data set is ${
+      Object.keys(lookup).length
+    }`,
+  );
+}
+
+main();
---
+++ typescript/src/utils.ts
@@ -0,0 +1,47 @@
+export const lines = (x: string) => {
+  return x.split(/\r?\n/);
+};
+
+type Fn = (str: string) => void;
+export const readLines = (data: string) => {
+  const l = data.split(/\r?\n/);
+  return (
+    {
+      extract: () => l.filter(Boolean),
+      map: (fn: Fn) => l.map(fn),
+    }
+  );
+};
+
+/**
+ * Perform breadth first search on a key/value data structure
+ * @param items
+ * @returns
+ */
+export const breadthFirstSearch: (
+  items: Record<string, string>,
+  initial: string,
+) => string[] = (
+  items,
+  initial,
+) => {
+  const queue: string[] = [initial];
+  const visited: string[] = [];
+
+  while (queue?.length) {
+    const current = queue.pop();
+
+    Object.entries(items).forEach(
+      ([k, v]) => {
+        if (visited.includes(k)) return;
+
+        if (v.includes(current || "")) {
+          queue.push(k);
+          visited.push(k);
+        }
+      },
+    );
+  }
+
+  return visited;
+};
