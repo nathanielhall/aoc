391f6fffe5de2b186f516ac8fb369d07beda01fe
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-11-18 06:47:14 -0500

    update template w/ parts

--- typescript/src/challenge-template.ts
+++ typescript/src/challenge-template.ts
@@ -1,6 +1,6 @@
 // import { readLines } from "../../utils.ts";
 
-const example: () => string = () => {
+const example1: () => string = () => {
   return (
     `
 	line 1
@@ -9,11 +9,28 @@ const example: () => string = () => {
   );
 };
 
-async function run() {
+async function part1() {
   // const text = await Deno.readTextFile("./input.txt");
-  const text = example();
+  const text = example1();
 
   //   const structure = readLines(text);
 }
 
-run();
+part1();
+
+// const example2: () => string = () => {
+//   return (
+//     `
+// 	line 1
+// 	line 2
+// 	`
+//   );
+// };
+// async function part2() {
+//   // const text = await Deno.readTextFile("./input.txt");
+//   const text = example2();
+
+//   //   const structure = readLines(text);
+// }
+
+// part2()
