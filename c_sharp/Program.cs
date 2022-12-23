00db0935a56b5f44c24efa8e8b71932c45106252
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-12-23 10:07:12 -0500

    C# AoC setup

---
+++ c_sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode
+{
+	class Program
+    {
+		static void Main(string[] args)
+		{
+			string input = File.ReadAllText(@"./example.txt");
+
+			var result = from elf in input.Split("\n\n") 
+				let calories = elf.Split('\n').Select(int.Parse).Sum()
+				orderby calories descending
+				select calories;
+
+				Console.WriteLine(result.First());
+		}
+	}
+}
+
+
+
