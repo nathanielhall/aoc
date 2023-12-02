8069d13ed34b68b96d081b5bfd15cf4791be534e
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2023-12-02 05:20:27 -0500

    AoC 2023: Day 1 - revise with regex

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -2,141 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"regexp"
 	"strconv"
 	"strings"
-	"unicode"
-
-	"github.com/nathanielhall/aoc/2023/utils"
 )
 
 func main() {
-	part1()
-	part2()
+	part1 := calc([]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	fmt.Printf("Part 1: %d \n", part1)
+	part2 := calc([]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	fmt.Printf("Part 2: %d \n", part2)
 }
 
-func part2() {
-	file, scanner, err := utils.ChallengeInput()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
+func calc(numbers []string) int {
+	input, _ := os.ReadFile("input.txt")
 	sum := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		nums := parseNumsSpelledOut(line)
-
-		num1 := strconv.Itoa(nums[0])
-		num2 := num1
-		if len(nums) > 1 {
-			num2 = strconv.Itoa(nums[len(nums)-1])
-		}
-
-		combinedStrings := num1 + num2
+	for _, line := range strings.Fields(string(input)) {
+		first, last := firstAndLastOccurance(line, numbers)
+		combinedStrings := strconv.Itoa(first) + strconv.Itoa(last)
 		result, _ := strconv.Atoi(combinedStrings)
-
 		sum += result
-
 	}
-	fmt.Printf("Part 1: %d \n", sum)
-	utils.CopyToClipboard(strconv.Itoa(sum))
+	return sum
 }
 
-func part1() {
-	file, scanner, err := utils.ChallengeInput()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	sum := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		nums := parseNums(line)
-
-		num1 := strconv.Itoa(nums[0])
-		num2 := num1
-		if len(nums) > 1 {
-			num2 = strconv.Itoa(nums[len(nums)-1])
-		}
-
-		combinedStrings := num1 + num2
-		result, _ := strconv.Atoi(combinedStrings)
-
-		sum += result
-
-	}
-	fmt.Printf("Part 2: %d \n", sum)
-	utils.CopyToClipboard(strconv.Itoa(sum))
-}
-
-func parseNums(line string) []int {
-	var numbers []int
-	for _, r := range line {
-		if unicode.IsDigit(r) {
-			s := string(r)
-			num, _ := strconv.Atoi(s)
-			numbers = append(numbers, num)
-		}
-	}
-	return numbers
-}
-
-func parseNumsSpelledOut(line string) []int {
-	var numbers = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+func firstAndLastOccurance(input string, numbers []string) (first int, last int) {
 	var lookup = map[string]int{"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "0": 0}
-	// var matches = map[string]int{}
-	type Match struct {
-		Key   string
-		Value int
-	}
-
-	var matches []Match
-
-	// numSpelling, index
-	for _, n := range numbers {
-		occurrences := findAllOccurrences(line, n)
-		for _, o := range occurrences {
-			matches = append(matches, Match{Key: n, Value: o})
-		}
-	}
-
-	// num, index
-	for i, r := range line {
-		if unicode.IsDigit(r) {
-			s := string(r)
-			matches = append(matches, Match{Key: s, Value: i})
-		}
-	}
-
-	firstOccurance := matches[0]
-	lastOccurance := matches[0]
-	for _, m := range matches {
-		if m.Value < firstOccurance.Value {
-			firstOccurance = m
-		}
-		if m.Value > lastOccurance.Value {
-			lastOccurance = m
-		}
-	}
-
-	return []int{lookup[firstOccurance.Key], lookup[lastOccurance.Key]}
-}
-
-func findAllOccurrences(s, substring string) []int {
-	var occurrences []int
-	index := -1
-	startIndex := 0
-
-	for {
-		index = strings.Index(s[startIndex:], substring)
-		if index == -1 {
-			break
-		}
-		occurrences = append(occurrences, startIndex+index)
-		startIndex += index + len(substring)
-	}
-
-	return occurrences
+	firstRegEx := regexp.MustCompile(`(` + strings.Join(numbers, "|") + `)`)
+	lastRegEx := regexp.MustCompile(`.*(` + strings.Join(numbers, "|") + `)`)
+	firstMatch := firstRegEx.FindStringSubmatch(input)[1]
+	lastMatch := lastRegEx.FindStringSubmatch(input)[1]
+	return lookup[firstMatch], lookup[lastMatch]
 }
--- 2023/go.mod
+++ 2023/go.mod
@@ -1,3 +1,5 @@
 module github.com/nathanielhall/aoc/2023
 
 go 1.19
+
+require golang.org/x/exp v0.0.0-20231127185646-65229373498e
